24798b49519273814f47d0f1477f73e5d1c30c72
Rafisyahhh <muhamadakbar1801@gmail.com>
2024-05-13 08:33:08 +0700

    user

---
+++ resources/views/perusahaan.blade.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Daftar Perusahaan - Victor Work</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+
+<body style="background-color:#f0fdf4">
+    <style>
+        .navbar {
+            z-index: 1;
+        }
+
+        .img-fluid {
+            filter: blur(4px);
+        }
+
+        .text-title {
+            position: absolute;
+            font-size: 80px;
+            left: 300px;
+            top: 208px;
+
+        }
+    </style>
+    <nav class="navbar navbar-expand-lg fixed-top" style="background-color:#4ade80;">
+        <div class="container">
+            <a class="navbar-brand text-light fw-bold" href="#">VICTOR WORK</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item mx-2">
+                        <a class="nav-link text-light" aria-current="page" href="/">Beranda</a>
+                    </li>
+                    <li class="nav-item mx-2">
+                        <a class="nav-link text-light" href="/lowongan">Lowongan Kerja</a>
+                    </li>
+                    <li class="nav-item mx-2">
+                        <a class="nav-link text-light active fw-bold" href="/perusahaan">Daftar Perusahaan</a>
+                    </li>
+                </ul>
+                <button class="btn btn-light fs-6 btn-sm text-light me-2 rounded-pill" style="background-color:#86efac">Masuk</button>
+                <button class="btn btn-light fs-6 btn-sm text-light rounded-pill" style="background-color:#86efac">Daftar</button>
+            </div>
+        </div>
+    </nav>
+    <img src="{{'image/pro.jpg'}}" class="img-fluid mt-4" alt="...">
+    <h1 class="fw-bold text-center text-title text-light">Daftar Perusahaan</h1>
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col">
+                
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+Route::get('/lowongan', function () {
+    return view('lowongan');
+});
+Route::get('/perusahaan', function () {
+    return view('perusahaan');
+});
+
