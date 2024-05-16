851792e8d9a03bc7103e570dc4141739b5bc2e03
Rafisyahhh <muhamadakbar1801@gmail.com>
2024-05-16 08:21:06 +0700

    perbaikan

--- database/migrations/2024_05_16_010824_lowongans.php
+++ database/migrations/2024_05_16_010824_lowongans.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('lowongans', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role', ['admin', 'user'])->default('user');
-            $table->rememberToken();
+            $table->foreignId('id_perusahaan');
+            $table->string('gaji');
+            $table->string('tempat_kerja');
+            $table->string('waktu_kerja');
+            $table->foreignId('id_posisi');
+            $table->string('ketentuan_kerja');
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        //
     }
 };
--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -3,32 +3,67 @@
 @extends('admin.master')
 
 @section('content')
-    <div class="container">
-        <h1>Daftar Pengguna</h1>
-        <a href="{{ route('user.create') }}" class="btn btn-primary mb-3">Tambah Pengguna</a>
-        <table class="table">
-            <thead>
-                <tr>
+<div class="card">
+    <div class="card-body">
+        <div class="d-flex justify-content-between">
+
+
+            <p class="fw-bold fs-5">Daftar Pengguna</p>
+            <a href="{{ route('user.create') }}" class="btn btn-primary ">Tambah Pengguna</a>
+        </div>
+
+        <div class="table-responsive">
+            <table class="table table-striped text-center  mb-0">
+
+                <thead>
+                    <tr>
                     <th>ID</th>
                     <th>Nama</th>
                     <th>Email</th>
                     <th>Role</th>
                     <th>Aksi</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($users as $user)
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach($user as $user)
                     <tr>
                         <td>{{ $user->id }}</td>
                         <td>{{ $user->name }}</td>
                         <td>{{ $user->email }}</td>
                         <td>{{ $user->role }}</td>
                         <td>
-                            <a href="{{ route('user.edit', $user->id) }}" class="btn btn-sm btn-primary">Edit</a>
+                            <a href="{{ route('user.edit', $user->id) }}" class="btn btn-warning"><i class="fa fa-pen"></i></a>
+                            <form action="{{ route('user.destroy', $user->id) }}" class="form-delete" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-danger" onclick="showConfirmation(this.parentNode)" type="button"><i
+                                class="fa fa-trash"></i></button>
+                            </form>
                         </td>
                     </tr>
                 @endforeach
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+        </div>
+
     </div>
+</div>
+<script>
+    function showConfirmation(form) {
+        Swal.fire({
+            title: 'Yakin ingin menghapus?',
+            text: "Tindakan ini tidak dapat dikembalikan!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#d33',
+            cancelButtonColor: '#3085d6',
+            confirmButtonText: 'Ya, hapus saja!',
+            cancelButtonText: 'Batal'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                form.submit();
+            }
+        });
+    }
+</script>
 @endsection
